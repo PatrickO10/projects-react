b1dd1d86506a5b06493d896fa86ec0f8addfb7c2
patricko10 <patrickorth10@gmail.com>
2018-04-07 14:15:40 -0500

    makes search bar for projects

--- src/ProjectsList.js
+++ src/ProjectsList.js
@@ -1,16 +1,45 @@
 import React, { Component } from 'react';
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
 
 // Data will be read-only so should use prop instead of state
 class ProjectsList extends Component {
+	state = {
+		query: ''
+	}
+
+	updateQuery = (query) => {
+		this.setState({query: query.trim()})
+	}
+
 	render() {
 		const { projects, section } = this.props
+		const { query } = this.state
+
+		let showingProjects
+
+		if (query) {
+			const match = new RegExp(escapeRegExp(query), 'i')
+			showingProjects = projects.filter((project) => match.test(project.title))
+		} else {
+			showingProjects = projects
+		}
+
+		showingProjects.sort(sortBy('title'))
 
 		return (
 
 			<div>
+				<input
+					className='search-projects'
+					type='text'
+					placeholder='Search Projects'
+					value={query}
+					onChange={(event) => this.updateQuery(event.target.value)}
+				/>
 				<h2>{section}</h2>
 				<ol>
-					{projects.map((project, index) => (
+					{showingProjects.map((project) => (
 						<li key={project.title}>
 							<h4>{project.title}</h4>
 							<img src="http://placeholder.pics/svg/300" alt="placeholder" />
