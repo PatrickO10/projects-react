bf4c4725db1837dd51fe5dfde11c9b2fd046883c
PatrickO10 <patrickorth10@gmail.com>
2018-04-24 13:29:28 -0500

    adds step three to readme.

--- README.md
+++ README.md
@@ -9,7 +9,8 @@ Following [Thinking in React](https://reactjs.org/docs/thinking-in-react.html) d
 ### Table of Contents
 1. [Starting With A Mock](#Starting-With-A-Mock)
 2. [Step One: Component Hierarchy](#Step-One:-Component-Hierarchy)
-3. 
+3. [Step Two: Build Static Version](#step-two-build-a-static-version-in-react)
+4. 
 
 ## Thinking in React
 
@@ -21,13 +22,13 @@ Following [Thinking in React](https://reactjs.org/docs/thinking-in-react.html) d
 
 ##### Technique: Single Responsibility Principle
 >* a component should ideally only [do one thing](https://en.wikipedia.org/wiki/Single_responsibility_principle). If it ends up growing, it should be decomposed into smaller subcomponents.
-
+>
 > * break it up into components that represent exactly one piece of your data model.
 
 #### Projects
-Front End Projects
-React Projects
-Full Stack Projects
+* Front End Projects
+* React Projects
+* Full Stack Projects
 
 1. **FilterableProjectTable (Orange)**: Contains the entirety of the projects
 2. **SearchBar (blue):** receives all user input
@@ -65,4 +66,45 @@ State should only be used for interactivity, aka data that changes over time.
   1. Start with building the components higher up in the hierarchy (FilterableProjectTable).
 * Bottom-up:
   1. Start with building the components lower in the hierarchy (ProjectRow). 
+
+
+#### A Brief Interlude: Props vs State
+
+>There are two types of “model” data in React: props and state. It’s important to understand the distinction between the two; skim the [official React docs](https://reactjs.org/docs/interactivity-and-dynamic-uis.html) if you aren’t sure what the difference is.
+
+### Step 3: Identify The Minimal (but complete) Representation Of UI State
+
+* Think of the minimal set of mutable **state** that your app needs.
+* [DRY: Don't Repeat Yourself.](https://en.wikipedia.org/wiki/Don%27t_repeat_yourself)
+* Compute everything else you need on-demand.
+
+> For example, if you’re building a TODO list, just keep an array of the TODO items around; don’t keep a separate state variable for the count. Instead, when you want to render the TODO count, simply take the length of the TODO items array.
+> 
+#### All the pieces of Data in the application:
+* The original list of projects
+* The search text the user has entered
+* The value of the checkbox
+* The filtered list of projects
+
+#### Go through each one and figure out whihc one is state by asking three questions about each piece of data:
+1. Is it passed in from a parent via props? If so, it probably isn't state.
+2. Does it remain unchanged over time? If so, it probably isn't state.
+3. Can you compute it based on any other state or props in your component? If so, it isn't state.
+
+* The original list of projects (**not state**)
+  - passed in as props 
+* The search text (**state**)
+  - changes over time
+  - cannot be computed from anything
+* The checkbox (**state**)
+  - changes over time
+  - cannot be computed from anything
+* The filtered list of projects (**not state**)
+  - can be computed by combining the original list of projects with the search text and value of the checkbox.
+
+**State:**
+* The search text the user has entered
+* The value of the checkbox
+
+
 
--- src/utils/data.js
+++ src/utils/data.js
@@ -4,49 +4,56 @@ export const PROJECTS = [{
         image: 'http://placeholder.pics/svg/300',
         description: 'Meet-Up Event Planner',
         demo: 'https://patricko10.github.io/meetUp',
-        source: 'https://github.com/PatrickO10/meetUp'
+        source: 'https://github.com/PatrickO10/meetUp',
+        completed: true
     }, {
         category: 'Front-End Projects',
         title: 'Neighborhood Map',
         image: 'http://placeholder.pics/svg/300',
         description: 'Neighborhood Map',
         demo: 'https://patricko10.github.io/neighborhood-map',
-        source: 'https://github.com/PatrickO10/neighborhood-map'
+        source: 'https://github.com/PatrickO10/neighborhood-map',
+        completed: true
     }, {
         category: 'Front-End Projects',
         title: 'Feed Reader Testing',
         image: 'http://placeholder.pics/svg/300',
         description: 'I enjoyed writing unit suites, testing one unit at a time.',
         demo: 'http://patricko10.github.io/frontend-nanodegree-feedreader/',
-        source: 'https://github.com/PatrickO10/frontend-nanodegree-feedreader'
+        source: 'https://github.com/PatrickO10/frontend-nanodegree-feedreader',
+        completed: true
     }, {
         category: 'Front-End Projects',
         title: 'Frogger Arcade',
         image: 'http://placeholder.pics/svg/300',
         demo: 'https://patricko10.github.io/froggerArcade',
         description: 'I enjoyed using the psuedoclassical class pattern. I especially liked my guardian subclass, which helps the hero score more points!',
-        source: 'https://github.com/PatrickO10/froggerArcade'
+        source: 'https://github.com/PatrickO10/froggerArcade',
+        completed: true
     }, {
         category: 'Front-End Projects',
         title: 'Website Optimization',
         image: 'http://placeholder.pics/svg/300',
         description: 'I enjoyed optimizing the pizza frame rate to 60 fps.  I removed slow code from the JavaScript file to unblock the rendering because if the code is still running in the call stack the page won\'t render.',
         demo: 'http://patricko10.github.io/udportfolio/',
-        source: 'https://github.com/PatrickO10/udportfolio'
+        source: 'https://github.com/PatrickO10/udportfolio',
+        completed: true
     }, {
         category: 'Front-End Projects',
         title: 'Interactive Resume',
         image: 'http://placeholder.pics/svg/300',
         description: 'I enjoyed playing around with Google Map. What made my project unique was when you click on an icon the background changes to a street view image and then the pano appears to the right of the map. I also added a recursive function to that process.Sometimes there wouldn\'t be a street view for the pano if the radius wasn\'t large enough, so I made a function call itself increasing the search area until it found one.',
         demo: 'https://patricko10.github.io/interactive-resume',
-        source: 'https://github.com/PatrickO10/interactive-resume'
+        source: 'https://github.com/PatrickO10/interactive-resume',
+        completed: true
     }, {
         category: 'Front-End Projects',
         title: 'Portfolio',
         image: 'http://placeholder.pics/svg/300',
         description: 'I enjoyed building a responsive webpage, using Backbone.js, bootstrap, underscore, jQuery. One of my favorite parts was adding two sets of projects.',
         demo: 'http://patricko10.github.io/',
-        source: 'https://github.com/PatrickO10/PatrickO10.github.io'
+        source: 'https://github.com/PatrickO10/PatrickO10.github.io',
+        completed: true
     },
     {
         category: 'Full Stack Projects',
@@ -54,19 +61,22 @@ export const PROJECTS = [{
         image: 'http://placeholder.pics/svg/300',
         description: 'I enjoyed picking several different movie trailers to add.',
         demo: 'https://patricko10.github.io/movie_trailers',
-        source: 'https://github.com/PatrickO10/movie_trailers'
+        source: 'https://github.com/PatrickO10/movie_trailers',
+        completed: true
     }, {
         category: 'Full Stack Projects',
         title: 'Tournament Planner',
         image: 'http://placeholder.pics/svg/300',
         description: 'I enjoyed creating a database for the first time using SQL. What was unique about my project is I created a helper method to manipulate the database in Python.',
-        source: 'https://github.com/PatrickO10/tournament_planner'
+        source: 'https://github.com/PatrickO10/tournament_planner',
+        completed: true
     }, {
         category: 'React Projects',
         title: 'My Books',
         image: 'http://placeholder.pics/svg/300',
         description: 'Book app',
         demo: 'https://patricko10.github.io/',
-        source: 'https://github.com/PatrickO10/books'
+        source: 'https://github.com/PatrickO10/books',
+        completed: true
     }
 ]
